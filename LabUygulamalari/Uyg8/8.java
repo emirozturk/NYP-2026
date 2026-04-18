6f74ea8d1c7d85e39684d2f7ef85f75117d306ce
Oğuz Kırat <okrt@users.noreply.github.com>
2026-04-18 15:34:27 +0300

    8. hafta uygulaması eklendi.

--- LabUygulamalari/Uyg8/8.java
+++ LabUygulamalari/Uyg8/8.java
@@ -1,17 +1,48 @@
+class KarmasikSayi{
+    private double gercel;
+    private double sanal;
+    public KarmasikSayi(double gercel,double sanal){
+        this.gercel=gercel;
+        this.sanal=sanal;
+    }
+    public double getGercel(){
+        return this.gercel;
+    }
+    public double getSanal(){
+        return this.sanal;
+    }
+    @Override
+    public String toString(){
+        return this.getGercel()+" + "+this.getSanal()+"i";
+    }
+}
+class Islemler{
+    public static double topla(double a,double b){
+        return a+b;
+    }
+    public static double topla(double[] dizi){
+        double toplam=0;
+        for (double eleman:dizi){
+            toplam+=eleman;
+        }
+        return toplam;
+    }
+    public static KarmasikSayi topla(KarmasikSayi a,KarmasikSayi b){
+        return new KarmasikSayi(a.getGercel()+b.getGercel(),a.getSanal()+b.getSanal());
+    }
+}
+
 public class Main {
     public static void main() {
         IO.println(Islemler.topla(3,4));
-        double[] s={3.5,.4};
+        double[] s={3.5,0.4};
         IO.println(Islemler.topla(s));
         KarmasikSayi k1=new KarmasikSayi(1,2);
         IO.print(
                 Islemler.topla(k1, new KarmasikSayi(3.2, 4.2))
         );
+
+
     }
-    /*
-    Beklenen çıktı
-    7.0
-    3.9
-    4.2 + 6.2i
-     */
+
 }
